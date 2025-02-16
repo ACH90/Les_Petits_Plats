694380700a57bf56ef15b13bb890ecf84001cc16
ACH90 <chassagne.alexandre@gmail.com>
2025-02-16 19:13:24 +0100

    DisplayOption OK

--- App.js
+++ App.js
@@ -361,43 +361,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
     displayOptions(ustensilOptionsContainer, ustensils, "ustensils");
   });
-
-  // optionElements.forEach((optionElement) => {
-  //   optionElement.addEventListener("click", () => {
-  //     const option = optionElement.textContent.toLowerCase();
-  //     console.log("Voici option", option);
-  //     // const optionList = optionElement.getAttribute();
-  //     selectOptions(optionElement);
-  //     // Mettre à jour l'affichage des recettes avec les nouveaux filtres
-  //     filteredRecipes = filterAndMapRecipes(
-  //       filteredRecipes,
-  //       mainSearchValue,
-  //       selectedIngredients,
-  //       selectedAppliances,
-  //       selectedUstensils
-  //     );
-  //     console.log("Voici filteredRecipes dans display", filteredRecipes);
-  //     renderRecipes(filteredRecipes);
-  //     updateOptions(ingredients, appliances, ustensils, filteredRecipes);
-
-  //     displayOptions(
-  //       ingredientOptionsContainer, // Conteneur pour les options des ingrédients
-  //       ingredients, // Liste des ingrédients
-  //       "ingredients" // La catégorie de filtre : ici 'ingredients'
-  //     );
-
-  //     displayOptions(
-  //       applianceOptionsContainer, // Conteneur pour les options des appareils
-  //       appliances, // Liste des appareils
-  //       "appliances" // La catégorie de filtre : ici 'appliances'
-  //     );
-
-  //     displayOptions(
-  //       ustensilOptionsContainer, // Conteneur pour les options des ustensiles
-  //       ustensils, // Liste des ustensiles
-  //       "ustensils" // La catégorie de filtre : ici 'ustensils'
-  //     );
-  //     handleOptionClick(optionElements);
-  //   });
-  // });
 });
